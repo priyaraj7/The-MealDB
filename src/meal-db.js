2d93c7c11f0154c3229d257297760f8332a041d3
Priyaraj7 <suppichala@gmail.com>
2020-03-26 09:21:40 -0700

    first commit

---
+++ src/meal-db.js
@@ -0,0 +1,21 @@
+export class MealDB {
+  async searchRecipe(name) {
+    try {
+      const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
+
+      let response = await fetch(url, {});
+
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+
+        console.log(jsonifiedResponse);
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch (e) {
+      return false;
+    }
+  }
+}
