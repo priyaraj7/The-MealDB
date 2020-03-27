0779f503203a4bd643163ee7684b336c9689911a
Priyaraj7 <suppichala@gmail.com>
2020-03-27 11:01:47 -0700

    delete Dotenv plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,38 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+//const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: "./src/main.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
-  devtool: 'eval-source-map',
+  devtool: "eval-source-map",
   devServer: {
-    contentBase: './dist'
+    contentBase: "./dist"
   },
   plugins: [
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'WeekDay',
-      template: './src/index.html',
-      inject: 'body'
-    }),
-    new Dotenv()
+      title: "WeekDay",
+      template: "./src/index.html",
+      inject: "body"
+    })
+    //new Dotenv()
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.js$/,
-        exclude: [
-            /node_modules/,
-            /spec/
-          ],
+        exclude: [/node_modules/, /spec/],
         loader: "eslint-loader"
       }
     ]
